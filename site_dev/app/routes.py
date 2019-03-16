8ac0363d779f6f74c7971ce4597a84b0d2bdbe20
testuser <testuser@example.com>
2019-03-16 13:24:06 +0200

    site json hanlding

--- site_dev/app/routes.py
+++ site_dev/app/routes.py
@@ -4,4 +4,14 @@ from flask import render_template
 @app.route("/")
 @app.route("/home")
 def index():
-  return render_template('index.html')
+  area_labels = ["January","February","March","April","May","June","July","August"]
+  area_data = [10,9,8,7,6,4,7,8]
+  context = {
+    "area_labels": area_labels,
+    "area_data": area_data,
+    "monthly_earnings": "50000",
+    "annual_earnings": "500000",
+    "data_usage": "75",
+    "sales_requests": "14",
+  }
+  return render_template('index.html', context=context)
--- site_dev/app/static/js/demo/chart-bar-demo.js
+++ site_dev/app/static/js/demo/chart-bar-demo.js
@@ -32,13 +32,18 @@ var ctx = document.getElementById("myBarChart");
 var myBarChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    // labels: ["January", "February", "March", "April", "May", "June"],
+    labels: [{% for item in bar_labels %}
+              "{{ item }}" ,
+    {{% endfor %}}],
     datasets: [{
       label: "Revenue",
       backgroundColor: "#4e73df",
       hoverBackgroundColor: "#2e59d9",
       borderColor: "#4e73df",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [{% for item in bar_data %}
+        {{ item }} ,
+{{% endfor %}}],
     }],
   },
   options: {
