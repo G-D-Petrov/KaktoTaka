3360cbbb9fb0e0c95a3eb2fda71bd7b2c1c14da9
G-D-Petrov <testuser@example.com>
2019-03-17 01:39:59 +0200

    Add Buy Data

--- site_dev/app/routes.py
+++ site_dev/app/routes.py
@@ -4,24 +4,24 @@ import json
 import ast 
 from flask import render_template, session, redirect, request, url_for
 
-def get_data():
-  r = requests.get('https://tus5jpx6z6.execute-api.eu-central-1.amazonaws.com/dev', data=json.dumps({"SensorType": "distance","top": 100}), timeout=30)
+def get_user_data():
+  r = requests.get('https://tus5jpx6z6.execute-api.eu-central-1.amazonaws.com/dev', data=json.dumps({"UserId": session['user']}), timeout=30)
   json_new = json.loads(r.text)
   data = ast.literal_eval(json_new['body'])
   count = 0
+  data_dict = {}
   labels_out = []
   data_out = []
   for line in data:
     count += 1
-    labels_out.append(count)
-    data_out.append(line[3])
+    if line[2] not in data_dict:
+      data_dict[line[2]] = 1
+    else:
+      data_dict[line[2]] += 1
 
-  return labels_out, data_out
+  return data_dict.keys(), data_dict.values()
 
-
-@app.route("/")
-@app.route("/home")
-def index():
+def get_home():
   if 'user' not in session:
     return redirect(url_for('login'))
 
@@ -29,11 +29,13 @@ def index():
     return redirect('/login')
   area_labels = ["January","February","March","April","May","June","July","August"]
   area_data = [10,9,8,7,6,4,7,8]
-  area_labels, area_data = get_data()
-  print(session['user'])
+  pie_labels, pie_data = get_user_data()
+
   context = {
     "area_labels": area_labels,
     "area_data": area_data,
+    "pie_labels": pie_labels,
+    "pie_data": pie_data,
     "monthly_earnings": "50000",
     "annual_earnings": "500000",
     "data_usage": "75",
@@ -41,14 +43,42 @@ def index():
     "label": "Distance Measurments",
     "username": session['user']
   }
+
+  return context
+
+def post_home():
+  json_to_zdravko = {
+    "UserId": session['user'],
+    "SensorType": request.form['sensorType'],
+    "NumberOfData": int(request.form['amount'])
+  }
+  r = requests.post('https://tus5jpx6z6.execute-api.eu-central-1.amazonaws.com/dev/transactions', data=json.dumps(json_to_zdravko))
+
+  json_new = json.loads(r.text)
+  data = ast.literal_eval(json_new['body'])
+  new_data = []
+  labels = []
+  count = 0
+  print(r.text)
+  for d in ast.literal_eval(data):
+    count += 1
+    labels.append(count)
+    new_data.append(d)
+
+  return labels, new_data
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+  context = get_home()
+  if request.method == 'POST':
+    context['area_labels'], context['area_data'] = post_home()
+  
   return render_template('index.html', context=context)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-  if 'user' in session:
-    if session['user'] is not None and session['user'] != "":
-      return redirect('/')
-  print(request.method)
   if request.method == 'POST':
     session['user'] = request.form['username']
     return redirect('/')
--- site_dev/app/static/js/demo/chart-pie-demo.js
+++ site_dev/app/static/js/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: pie_labels,
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: pie_data,
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc', '#36b94c', '#3629cc', '#36b91c'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf', '#4e73df', '#1cc88a', '#36b9cc',],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
