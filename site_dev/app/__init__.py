b98e4b81a352d57d52f3b331395214640a0805de
G-D-Petrov <testuser@example.com>
2019-03-16 23:12:09 +0200

    Add Login

--- site_dev/app/__init__.py
+++ site_dev/app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+app.secret_key = "120893u1023821-3183-2103"
 from app import routes
--- site_dev/app/routes.py
+++ site_dev/app/routes.py
@@ -2,10 +2,10 @@ from app import app
 import requests
 import json
 import ast 
-from flask import render_template
+from flask import render_template, session, redirect, request, url_for
 
 def get_data():
-  r = requests.get('https://tus5jpx6z6.execute-api.eu-central-1.amazonaws.com/dev', data=json.dumps({"SensorType": "distance","top": 10}))
+  r = requests.get('https://tus5jpx6z6.execute-api.eu-central-1.amazonaws.com/dev', data=json.dumps({"SensorType": "distance","top": 100}), timeout=30)
   json_new = json.loads(r.text)
   data = ast.literal_eval(json_new['body'])
   count = 0
@@ -22,11 +22,15 @@ def get_data():
 @app.route("/")
 @app.route("/home")
 def index():
-  r = requests.get('https://tus5jpx6z6.execute-api.eu-central-1.amazonaws.com/dev')
-  print(r.text)
+  if 'user' not in session:
+    return redirect(url_for('login'))
+
+  if not session['user']:
+    return redirect('/login')
   area_labels = ["January","February","March","April","May","June","July","August"]
   area_data = [10,9,8,7,6,4,7,8]
   area_labels, area_data = get_data()
+  print(session['user'])
   context = {
     "area_labels": area_labels,
     "area_data": area_data,
@@ -34,5 +38,18 @@ def index():
     "annual_earnings": "500000",
     "data_usage": "75",
     "sales_requests": "14",
+    "label": "Distance Measurments",
+    "username": session['user']
   }
   return render_template('index.html', context=context)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if 'user' in session:
+    if session['user'] is not None and session['user'] != "":
+      return redirect('/')
+  print(request.method)
+  if request.method == 'POST':
+    session['user'] = request.form['username']
+    return redirect('/')
+  return render_template('login.html')
